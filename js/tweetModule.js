c515c3476f5190c2c971c57335f9c34f56cd8786
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-08-16 10:42:16 -0400

    Created Tweets controller with constructor

--- js/tweetModule.js
+++ js/tweetModule.js
@@ -2,33 +2,33 @@
 var tweetApp = angular.module("tweetApp", ['ngRoute']);
 
 
-tweetApp.directive('customMenu', function(){
-	return {
-		restrict: 'E', //E = element, A = attribute, C =  class, M = Comment (dont use comment)
-		templateUrl: "views/menu.html"
-	}
-});
+// tweetApp.directive('customMenu', function(){
+// 	return {
+// 		restrict: 'E', //E = element, A = attribute, C =  class, M = Comment (dont use comment)
+// 		templateUrl: "views/menu.html"
+// 	}
+// });
 
 
-tweetApp.config(function($routeProvider){
-	$routeProvider.when('/',{ //at /, load home.html with home controller
-		templateUrl: 'views/home.html',
-		controller: 'homeController'
-	});
-	$routeProvider.when('/trump',{
-		templateUrl: 'views/home.html',
-		controller: 'trumpController'
-	});
-	$routeProvider.when('/hillary',{ // at /hillary, load same template but new controller
-		templateUrl: 'views/home.html',
-		controller: 'hillaryController'
-	});
-	$routeProvider.otherwise('/'); //default
+// tweetApp.config(function($routeProvider){
+// 	$routeProvider.when('/',{ //at /, load home.html with home controller
+// 		templateUrl: 'views/home.html',
+// 		controller: 'homeController'
+// 	});
+// 	$routeProvider.when('/trump',{
+// 		templateUrl: 'views/home.html',
+// 		controller: 'trumpController'
+// 	});
+// 	$routeProvider.when('/hillary',{ // at /hillary, load same template but new controller
+// 		templateUrl: 'views/home.html',
+// 		controller: 'hillaryController'
+// 	});
+// 	$routeProvider.otherwise('/'); //default
 
-});
+// });
 
-this.tweeter1 = function(){
-	this.tweet = "";
-};
+// this.tweeter1 = function(){
+// 	this.tweet = "";
+// };
 
 
